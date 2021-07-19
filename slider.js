85ccc0257acf3abc8d86294e0fc89cbace23d09a
SeKaMo <polozhentsew02@gmail.com>
2021-07-19 17:28:59 +0300

    Added task 3

---
+++ slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+    $('.myslider').slick({
+
+        infinite: true,
+        focusOnSelect: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplay: true,
+        fade: true,
+        autoplaySpeed: 4000,
+    });
+});
